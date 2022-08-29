84ee659bf6f7d738be2906a43146eebb35de3a38
Fabrizio <fabgrel10@gmail.com>
2022-08-29 16:34:48 -0500

    🔨 Implement API call in Board component

--- src/pages/Board/Board.jsx
+++ src/pages/Board/Board.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import Lane from '../../components/Lane/Lane';
 
 import './Board.css';
@@ -10,10 +12,39 @@ const lanes = [
 ];
 
 function Board() {
+  const [loading, setLoading] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const apiTasks = await fetch(
+          `https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`
+        );
+        const result = await apiTasks.json();
+
+        if (result) {
+          setTasks(result);
+          setLoading(false);
+        }
+      } catch (e) {
+        setError(error);
+        setLoading(false);
+      }
+    }
+  }, [error]);
+
   return (
     <div className="Board-wrapper">
       {lanes.map(lane => (
-        <Lane key={lane.id} title={lane.title} />
+        <Lane
+          key={lane.id}
+          title={lane.title}
+          loading={loading}
+          error={error}
+          tasks={tasks.filter((task) => task.lane === lane.id)}
+        />
       ))}
     </div>
   );
