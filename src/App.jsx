02413574d944cccd600c58ef866b533b59965f4b
Fabrizio <fabgrel10@gmail.com>
2022-08-28 22:02:35 -0500

    🎉 Initialize project and setup repository

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header/Header';
+import Board from './pages/Board/Board';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Board />
+    </div>
+  );
+}
+
+export default App;
