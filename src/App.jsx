4ff0eeb13f92c2971693533dd4e87ecd17b70885
Fabrizio <fabgrel10@gmail.com>
2022-08-29 21:56:58 -0500

    🚚 Implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header/Header';
 import Backlog from './pages/Backlog/Backlog';
 import Board from './pages/Board/Board';
+import Landing from './pages/Landing/Landing';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-      <Board />
-      <Backlog />
-    </div>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="board" element={<Board />} />
+        <Route path="backlog" element={<Backlog />} />
+      </Routes>
+    </>
   );
 }
 
